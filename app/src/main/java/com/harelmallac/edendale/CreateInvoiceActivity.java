d2b87fb584e08bb1c96ab9445159590559b0f0e1
alexandreb <alexandreb@harrelmallac.com>
2020-09-30 15:52:51 +0400

    Creation of createInvoice files Listview for customer + layout for list view

--- app/src/main/java/com/harelmallac/edendale/CreateInvoiceActivity.java
+++ app/src/main/java/com/harelmallac/edendale/CreateInvoiceActivity.java
@@ -3,12 +3,40 @@ package com.harelmallac.edendale;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class CreateInvoiceActivity extends AppCompatActivity {
+import java.text.DateFormatSymbols;
+
+public class CreateInvoiceActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    ListView LV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_invoice);
+
+        LV = findViewById(R.id.cusListView);
+
+        String[] months = new DateFormatSymbols().getMonths();
+        //ArrayAdapter<String> cusListViewAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, months);
+        ArrayAdapter<String> cusListViewAdapter = new ArrayAdapter<>(this, R.layout.customer_list_view_layout, months);
+        LV.setAdapter(cusListViewAdapter);
+        LV.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String month = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getApplicationContext(),month,Toast.LENGTH_SHORT).show();
     }
 }
